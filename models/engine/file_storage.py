37d17671dea4170a0f8cccfc400fd3a93c58a9a8
Juanesduque1 <juanesduquemorales@gmail.com>
2023-03-09 15:37:18 -0500

    [Fix]: file_storage.py reload method fixed
    Co-authored-by: felipevcc <felipevillamizarc@gmail.com>

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -31,7 +31,14 @@ class FileStorage:
     def reload(self):
         """Deserializes the JSON file to __objects' dictionary'"""
         from models.base_model import BaseModel
+        from models.amenity import Amenity
+        from models.city import City
+        from models.user import User
+        from models.place import Place
+        from models.review import Review
+        from models.state import State
+
         if path.exists(self.__file_path):
             with open(self.__file_path, "r") as file:
-                for _, value in json.loads(file.read()).items():
-                    self.new(BaseModel(**value))
+                for value in json.loads(file.read()).values():
+                    eval(value["__class__"])(**value)
